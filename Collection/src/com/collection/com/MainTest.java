38df6d7e08028da6f0ca634314bf36c906d5950d
Nehru Mariapppan <nehrump@github.com>
2018-12-30 21:29:26 +0530

    New Collection Test

---
+++ Collection/src/com/collection/com/MainTest.java
@@ -0,0 +1,15 @@
+package com.collection.com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainTest {
+
+	public static  void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+list.add("one");
+list.add("two");
+System.out.println(list);
+	}
+
+}
