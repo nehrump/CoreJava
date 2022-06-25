f3d83fab2edf203c6fb33c4c303ad30b6f823c79
nehru.mp <nehru.mp@gmail.com>
2022-06-25 08:58:50 +0530

    Observable Pattern Demo program

--- src/com/nehru/designpatterns/behavioral/ObservablePatternDemo.java
+++ src/com/nehru/designpatterns/behavioral/ObservablePatternDemo.java
@@ -1,4 +1,74 @@
 package com.nehru.designpatterns.behavioral;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ObservablePatternDemo {
+    public static void main(String[] args){
+        NotificationHandler notificationHandler=new NotificationHandler();
+
+        User user=new User();
+        Seller seller=new Seller();
+        Warehouse warehouse=new Warehouse();
+
+        notificationHandler.registerObserver(user);
+        notificationHandler.registerObserver(seller);
+        notificationHandler.registerObserver(warehouse);
+
+        notificationHandler.notifyObservers();
+
+        notificationHandler.deRegisterObserver(warehouse);
+        notificationHandler.notifyObservers();
+
+    }
+}
+
+
+interface Observer {
+    void update();
+}
+
+class User implements Observer {
+    @Override
+    public void update() {
+        System.out.println("User info updated");
+    }
+}
+
+class Seller implements Observer {
+    @Override
+    public void update() {
+        System.out.println("Seller info updated");
+    }
+}
+
+class Warehouse implements Observer {
+    @Override
+    public void update() {
+        System.out.println("Warehouse info updated");
+    }
+}
+
+class NotificationHandler {
+    List<Observer> observers;
+
+    NotificationHandler() {
+        observers = new ArrayList<>();
+    }
+
+    void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    void deRegisterObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+
 }
