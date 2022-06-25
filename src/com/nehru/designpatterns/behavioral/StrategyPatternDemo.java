adf10525952e740acbab3df787b9c35fa1b08c2e
nehru.mp <nehru.mp@gmail.com>
2022-06-25 20:23:22 +0530

    StrategyPatternDemo added

---
+++ src/com/nehru/designpatterns/behavioral/StrategyPatternDemo.java
@@ -0,0 +1,53 @@
+package com.nehru.designpatterns.behavioral;
+
+interface Strategy {
+
+  void sort();
+}
+
+public class StrategyPatternDemo {
+
+  public static void main(String[] args) {
+    EmployeeCollection employeeCollection = new EmployeeCollection(new BubbleSort());
+    employeeCollection.sort();
+
+    employeeCollection.setStrategy(new QuickSort());
+    employeeCollection.sort();
+  }
+
+}
+
+class EmployeeCollection {
+
+  Strategy strategy;
+
+  public EmployeeCollection(Strategy strategy) {
+    this.strategy = strategy;
+  }
+
+  public void setStrategy(Strategy strategy) {
+    this.strategy = strategy;
+  }
+
+  public void sort() {
+    this.strategy.sort();
+  }
+
+
+}
+
+class QuickSort implements Strategy {
+
+  @Override
+  public void sort() {
+    System.out.println("Quick sort is done");
+  }
+}
+
+class BubbleSort implements Strategy {
+
+  @Override
+  public void sort() {
+    System.out.println("Bubble sort is done");
+  }
+}
