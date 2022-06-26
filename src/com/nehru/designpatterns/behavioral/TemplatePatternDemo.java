d5de21b3927b66b4f935a6e8f2e281e0cae589f1
nehru.mp <nehru.mp@gmail.com>
2022-06-26 19:37:54 +0530

    TemplatePatternDemo added

---
+++ src/com/nehru/designpatterns/behavioral/TemplatePatternDemo.java
@@ -0,0 +1,72 @@
+package com.nehru.designpatterns.behavioral;
+
+/**
+ * The type Template pattern - defines template of algorithm and allows subclass to redefine the
+ * steps of algorithm without changing the structure
+ */
+public class TemplatePatternDemo {
+
+  public static void main(String[] args) {
+
+    Game coc = new ClashOfClans();
+    coc.play();
+
+    Game freeFire = new FreeFire();
+    freeFire.play();
+
+  }
+
+}
+
+abstract class Game {
+
+  abstract void initialise();
+
+  abstract void start();
+
+  abstract void end();
+
+  void play() {
+    initialise();
+    start();
+    end();
+  }
+
+}
+
+class ClashOfClans extends Game {
+
+  @Override
+  void initialise() {
+    System.out.println("Clash of clans is loading");
+  }
+
+  @Override
+  void start() {
+    System.out.println("Clash of clans started");
+  }
+
+  @Override
+  void end() {
+    System.out.println("Clash of clans ended");
+  }
+}
+
+class FreeFire extends Game {
+
+  @Override
+  void initialise() {
+    System.out.println("FreeFire is loading");
+  }
+
+  @Override
+  void start() {
+    System.out.println("FreeFire started");
+  }
+
+  @Override
+  void end() {
+    System.out.println("FreeFire ended");
+  }
+}
+
